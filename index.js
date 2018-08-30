48a7ca6a59c264ef14262e14ee3d0c010f7d27a5
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-08-30 07:38:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  const select = document.querySelector(selector)
+  return select;
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for(var i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var test = document.getElementById('grand-node').querySelectorAll('div')
+  var deep;
+  for(var j=0; j=test.length-1; j++){
+    deep = test[j].querySelector('div')
+  }
+  return deep;
+}
